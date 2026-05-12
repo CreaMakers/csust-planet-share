5667dc46e352de9bcf3860fc653da9a9f5f763fc
zHElEARN <personal@zhelearn.com>
2026-05-12 11:48:26 +0800

    feat: 配置site-url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
 
 import tailwindcss from "@tailwindcss/vite";
 
+const env = loadEnv(process.env.NODE_ENV ?? "production", process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
+  site: env.PUBLIC_SITE_URL,
   output: "static",
   vite: {
     plugins: [tailwindcss()],
