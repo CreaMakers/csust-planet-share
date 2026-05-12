9b0641d29d36381f327e1fb35c4d0c3e5ada3ddf
zHElEARN <personal@zhelearn.com>
2026-05-12 15:39:59 +0800

    feat: 添加sitemap和robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import { loadEnv } from "vite";
 
 import tailwindcss from "@tailwindcss/vite";
@@ -9,6 +10,7 @@ const env = loadEnv(process.env.NODE_ENV ?? "production", process.cwd(), "");
 export default defineConfig({
   site: env.PUBLIC_SITE_URL,
   output: "static",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
